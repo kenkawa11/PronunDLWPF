0ea68c2b07248323ce46165e92c1bc2df199e9d3
kenkawa <naobaby@me.com>
2020-06-03 17:32:19 +0900

    master

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+//using PronunDLengine;
+using System.Diagnostics;
+using System.Security.Policy;
+using PronunEngine;
+
+namespace PronunDLWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fn;
+        //public static HttpClient client = new HttpClient();
+        Boolean isCancel;
+        public processViewModel vm=new processViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fd = new OpenFileDialog();
+
+            fd.FileName = "";
+            fd.DefaultExt = "*.*";
+            if (fd.ShowDialog() == true)
+            {
+                fn = fd.FileName;
+                txtifile.Text = fn;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           // Engine.fntreat(fn);
+            fntreat(txtifile.Text);
+        }
+
+        public class processViewModel : INotifyPropertyChanged
+        {
+            private string status;
+            private string progress;
+            private int barProgress;
+
+
+            public string Status
+            {
+                get
+                {
+                    return this.status;
+                }
+                set
+                {
+                    this.status = value;
+                    NotifyPropertyChanged("Status");
+                }
+            }
+
+            public string Progress
+            {
+                get
+                {
+                    return this.progress;
+                }
+                set
+                {
+                    this.progress = value;
+                    NotifyPropertyChanged("Progress");
+
+                }
+            }
+
+            public int BarProgress
+            {
+                get
+                {
+                    return this.barProgress;
+                }
+                set
+                {
+                    this.barProgress = value;
+                    NotifyPropertyChanged("BarProgress");
+
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged;
+            private void NotifyPropertyChanged(String info)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(info));
+                }
+            }
+        }
+
+
+
+        public async void fntreat(string rfn)
+        {
+            //label1.Content = "Processing";
+            vm.Status = "Processing";
+            var LoadFileData = new fileData(rfn);
+            var num_treat = 0;
+            var num_gross = LoadFileData.Fdata.Count;
+            var linedata = new ID_data();
+            foreach (var values in LoadFileData.Fdata)
+            {
+                linedata.Line = values;
+                values[3] = linedata.treatMp3();
+
+                values[4] = linedata.treatSym();
+
+                num_treat = num_treat + 1;
+                vm.BarProgress = num_treat*100 / num_gross ;
+                vm.Progress = $"{num_treat} in {num_gross}";
+
+
+                if (isCancel)
+                {
+                    break;
+                }
+
+                if (num_treat < num_gross)
+                {
+                    await Task.Delay(1000);
+                }
+            }
+
+            vm.Status = "Writing data in file";
+            LoadFileData.writeData();
+            vm.Status= "Complete";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            isCancel = true;
+        }
+
+        private void Window_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy; // マウスカーソルをコピーにする。
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+
+
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            txtifile.Text = string.Empty; // テキストボックスを空にする。
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop); // ドロップされたものがFileDrop形式の場合は、各ファイルのパス文字列を文字列配列に格納する。
+            if (files != null)
+            {
+
+                txtifile.Text = files[0];// パス文字列からファイル名を抜き出して、テキストボックスにファイル名を書き込む。
+                
+                
+            }
+
+        }
+    }
+
+
+
+
+}
---
+++ engine.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+using System.Dynamic;
+using System.Windows.Shapes;
+
+namespace PronunEngine
+{
+    public class dic
+    {
+        public static string outbase = @"C:\Users\naobaby\Desktop\test\";
+        public static HttpClient client = new HttpClient();
+        public string url { get; set; }
+        public string ptn { get; set; }
+
+        public dic(string a, string b)
+        {
+            url = a;
+            ptn = b;
+        }
+
+        public static async void get_mp3(string url_mp3, string fn)
+        {
+            HttpResponseMessage res = await client.GetAsync(url_mp3);
+            var outputPath = outbase + fn + ".mp3";
+            using (var fileStream = File.Create(outputPath))
+            {
+                using (var httpStream = await res.Content.ReadAsStreamAsync())
+                {
+                    httpStream.CopyTo(fileStream);
+                    fileStream.Flush();
+                }
+            }
+        }
+
+
+
+        public string DownLoadMp3(string w, string ID)
+        {
+            var bodyUrl = url + w;
+            var url_mp3 = get_body(bodyUrl, ptn).Result;
+            if (url_mp3 == "0")
+            {
+                return "0";
+            }
+
+            if (url == "https://ejje.weblio.jp/content/")
+            {
+                url_mp3 = url_mp3.Substring(0, url_mp3.Length - 1);
+            }
+            get_mp3(url_mp3, ID);
+            return "1";
+        }
+
+        public string DownLoadSymbol(string w)
+        {
+            var bodyUrl = url + w;
+            var url_symbol = get_body(bodyUrl, ptn).Result;
+            if (url_symbol == "0")
+            {
+                return "0";
+            }
+
+
+            var pos = url_symbol.IndexOf("】");
+
+            return url_symbol.Substring(pos + 1, url_symbol.Length - pos - 5);
+        }
+
+        public static async Task<string> get_body(string url, string ptn)
+        {
+            try
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+                var html = await client.GetStringAsync(url).ConfigureAwait(false);
+                var reg = new Regex(ptn);
+                var m = reg.Match(html);
+                if (m.Success)
+                {
+                    return m.Groups[0].Value;
+                }
+                return "0";
+            }
+            catch (HttpRequestException e)
+            {
+                return "0";
+            }
+        }
+    }
+
+    public class ID_data
+    {
+
+        private List<string> line;
+        public List<string> Line
+        {
+            get
+            {
+                return this.line;
+            }
+            set
+            {
+                this.line = value;
+            }
+        }
+        
+        
+        public static dic oxford = new dic("https://www.oxfordlearnersdictionaries.com/definition/english/",
+            "https://www.oxfordlearnersdictionaries.com/media/english/us_pron/.+?mp3");
+
+        public static dic longman = new dic("https://www.ldoceonline.com/jp/dictionary/",
+            "https://.+/ameProns/.+?mp3");
+
+        public static dic weblio = new dic("https://ejje.weblio.jp/content/","https://weblio.hs.llnwd.net/.+?mp3\"");
+
+        public static dic ejiro = new dic("https://eow.alc.co.jp/", "【発音】.+?【カナ】|【発音.+?】.+?【カナ】");
+
+
+
+        public string treatMp3()
+        {
+            if(line[3]=="n")
+            {
+                var target_word = line[2];
+                target_word = target_word.Trim().Replace(" ", "+");
+                if (oxford.DownLoadMp3(target_word, line[0]) != "0")
+                {
+                    return "A";
+                }
+                if (longman.DownLoadMp3(target_word, line[0]) != "0")
+                {
+                    return "A";
+                }
+                if (weblio.DownLoadMp3(target_word, line[0]) != "0")
+                {
+                    return "A";
+                }
+                return "n";
+            }
+            return line[3];
+
+        }
+
+        public string treatSym()
+        {
+            if (line[4] == "n")
+            {
+                var target_word = line[2];
+                target_word = target_word.Trim().Replace(" ", "+");
+                target_word = "search?q=" + target_word;
+                var temp = ejiro.DownLoadSymbol(target_word);
+                if (temp == "0")
+                {
+                    return  "n";
+
+                }
+                else
+                {
+                    return temp;
+                }
+            }
+            return line[4];
+
+        }
+
+
+    }
+
+
+    public class fileData
+    {
+        private List<List<string>> fdata = new List<List<string>>();
+        public string Rfn { get; set; }
+        private int dataNum { get; set; }
+
+        public List<List<string>> Fdata
+        {
+            get
+            {
+                return this.fdata;
+            }
+            set
+            {
+                this.fdata = value;
+
+            }
+        }
+
+        public fileData(string rfn)
+        {
+            Rfn = rfn;
+            string line;
+            String[] readLine;
+
+            using (StreamReader sr = new StreamReader(rfn))
+            {
+
+                while (!sr.EndOfStream) //csvファイルをリストに読み込む
+                {
+                    // CSVファイルの一行を読み込む
+                    List<string> stringList = new List<string>();
+                    line = sr.ReadLine();
+                    // 読み込んだ一行をカンマ毎に分けて配列に格納する
+                    readLine = line.Split(',');
+                    stringList.AddRange(readLine);
+                    Fdata.Add(stringList);
+                    readLine = null;
+                }
+            }
+
+            dataNum = Fdata.Count;
+        }
+
+        public void writeData()
+        {
+            string line = null;
+            using (StreamWriter file = new StreamWriter(this.Rfn, false, Encoding.UTF8))
+            {
+                foreach (var v in this.Fdata)
+                {
+                    foreach (var u in v)
+                    {
+                        line += u;
+                        line += ",";
+                    }
+                    line = line.Substring(0, line.Length - 1);
+                    file.WriteLine(line);
+                    line = null;
+                }
+            }
+        }
+    }
+}
